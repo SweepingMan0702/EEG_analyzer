166202127f58acca476bf3dbee5f5b4e60e4f36e
SweepingMan0702 <blackfire.0976@gmail.com>
2024-09-16 15:13:36 +0800

    9/16

--- 切割後/movedel.m
+++ 切割後/movedel.m
@@ -1,14 +1,19 @@
 %使用說明 *移除體動 (前後名平均取代標記點)
 %資料需求: 選擇一資料夾內包含{base,fatigue,recovered}資料夾且內部需有.mat檔
-%將10、85取消註解
 %執行後原路徑生成"體動移除"資料夾，包含Cz、Fz等的ps資料(.mat共六個檔案)
 %
-
+save_switch = 0;
 fpath = uigetdir(pwd, 'Select a folder');
 list = {'base','fatigue','recovered'};
 channel = {'Cz','Fz'};
 % mkdir([fpath '\體動移除']);
-over_value = 0.4;
+
+if save_switch == 1
+    mkdir([fpath '\體動移除']);
+end
+
+% 超過點能量大小 、 超過點個數
+over_value = 0.2;
 over_counts = 3;
 for k = 1:length(list)
     temp_path = [fpath '\' list{k}];
@@ -33,7 +38,6 @@ for k = 1:length(list)
             nfft = 256;
             [~, f, t_stft, ps] = spectrogram(x, win, noverlap, nfft, fs, "ps");
             [r, c] = size(ps);
-            ps = process_array(ps, over_value, over_counts);
             finish = 0;
             count = 0;
             subplot(2,1,1);
@@ -49,21 +53,21 @@ for k = 1:length(list)
             ylabel(colorbar, '振幅');
             set(gcf, 'Units', 'Inches', 'Position', [0, 0, 16, 9]);
 
-            while finish == 0 && count < 1000
-                count = count + 1;
-                finish = 1;
-                for cs = 1:c
-                    % 檢查30~50行中是否有3個值超過0.2
-                    if sum(ps(30:50, cs) > over_value) >= over_counts
-                        finish = 0;
-                        break;
-                    end
-                end
-                if finish == 0
-                    ps = process_array(ps, over_value, over_counts);
-                end
-            end
-
+            % while finish == 0 && count < 1000
+            %     count = count + 1;
+            %     finish = 1;
+            %     for cs = 1:c
+            %         % 檢查30~50行中是否有3個值超過0.2
+            %         if sum(ps(30:50, cs) > over_value) >= over_counts
+            %             finish = 0;
+            %             break;
+            %         end
+            %     end
+            %     if finish == 0
+            %         ps = process_array(ps, over_value, over_counts);
+            %     end
+            % end
+            ps = process_array(ps, over_value, over_counts,remove_counts);
             
             subplot(2,1,2);
             surf(t_stft/60,f, ps, 'EdgeColor', 'none');
@@ -82,14 +86,18 @@ for k = 1:length(list)
             result_file_name = sprintf([list{k} '_' channel{ch} '.mat']);
             full_path = fullfile([fpath '\體動移除'] , result_file_name);
             % 保存這個時間段的數據
-            % save(full_path, 'ps','t_stft','f');
+            if save_switch == 1
+                save(full_path, 'ps','t_stft','f');
+            end
+
         end
-        clearvars -except fpath list channel temp_path matFiles over_value over_counts;
+        clearvars -except fpath list channel temp_path matFiles over_value over_counts save_switch;
     end
 end
 
 
-function result = process_array(input_array, over_value, over_counts)
+function result = process_array(input_array, over_value, over_counts,remove_col)
+    remove_col  = 0;
     % 獲取數組的大小
     [rows, cols] = size(input_array);
 
@@ -101,16 +109,18 @@ function result = process_array(input_array, over_value, over_counts)
         % 檢查30~50行中是否有3個值超過0.2
         if sum(input_array(30:50, col) > over_value) >= over_counts
             for row = 1:rows
-                % 計算左右兩個數的平均值
-                if col == 1
-                    avg = (input_array(row, 2) + input_array(row, end)) / 2;
-                elseif col == cols
-                    avg = (input_array(row, 1) + input_array(row, end-1)) / 2;
-                else
-                    avg = (input_array(row, col-1) + input_array(row, col+1)) / 2;
-                end
+                % 本來取平均，修改成直接取代 
+                % % 計算左右兩個數的平均值
+                % if col == 1
+                %     avg = (input_array(row, 2) + input_array(row, end)) / 2;
+                % elseif col == cols
+                %     avg = (input_array(row, 1) + input_array(row, end-1)) / 2;
+                % else
+                %     avg = (input_array(row, col-1) + input_array(row, col+1)) / 2;
+                % end
                 % 替換值
-                result(row, col) = avg;
+                remove_col = remove_col + 1;
+                result(row, col) = 0;
             end
         end
     end
